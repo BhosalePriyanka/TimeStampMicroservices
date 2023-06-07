848387796e9bceaf21c729c2bfe3df74068031ea
BhosalePriyanka <priyanka.patil970@gmail.com>
2023-06-07 21:45:09 +0100

    project added

--- index.js
+++ index.js
@@ -5,6 +5,9 @@
 var express = require('express');
 var app = express();
 
+//env 
+require('dotenv').config()
+
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
@@ -23,10 +26,43 @@ app.get("/", function (req, res) {
 app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
+let resObject = {}
+app.get('/api',(req,res)=>{
+  resObject["unix"] = new Date().getTime()
+  resObject["utc"] = new Date().toUTCString()
+  res.json(resObject)
+})
 
+app.get("/api/:date",(req,res)=>{
+
+  let input = req.params.date
+ 
+  const dateStringRegex = /^[0-9]+$/
+  const numbersOnly = dateStringRegex.test(input)
+ 
+  
+ if(!numbersOnly){
+  resObject["unix"] =  new Date(input).getTime() 
+  resObject["utc"] = new Date(input).toUTCString()
+  
+}
+else{
+ input = parseInt(input)
+resObject["unix"] =  new Date(input).getTime() 
+resObject["utc"] = new Date(intInput).toUTCString()
+}
+  if(!resObject["unix"] || !resObject["utc"]){
+    res.json({error:"Invalid Date"})
+  }
+res.json(resObject)
+})
 
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
-});
+// var listener = app.listen(process.env.PORT, function () {
+//   console.log('Your app is listening on port ' + listener.address().port);
+// });
+
+app.listen(3000,()=>{
+  console.log(`Connected to Server`)
+})
